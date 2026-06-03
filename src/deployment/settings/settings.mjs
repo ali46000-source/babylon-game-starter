4a21758adaaa7c92cc08a6fd3f028ec35df1e66e
ali46000-source <ali46000@auburnschl.edu>
2026-06-03 09:39:45 -0400

    netlify

--- src/deployment/settings/settings.mjs
+++ src/deployment/settings/settings.mjs
@@ -1,6 +1,6 @@
-/** @type {import('../types/settings').DeploymentSettings<'github.io'>} */
+/** @type {import('../types/settings').DeploymentSettings<'netlify'>} */
 const deploymentSettings = {
-  host: 'github.io',
+  host: 'netlify',
   type: 'static',
   services: [
     {
@@ -11,8 +11,7 @@ const deploymentSettings = {
     }
   ],
   static: {
-    basePath: '/babylon-game-starter/',
-    publicUrl: 'https://ericeisaman.github.io/babylon-game-starter/'
+    basePath: '/'
   }
 };
 
