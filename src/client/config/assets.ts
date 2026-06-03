d98aacb5ee29db2bd4ce966120fc1229e43561a5
ali46000-source <ali46000@auburnschl.edu>
2026-06-03 10:05:31 -0400

    speed

--- src/client/config/assets.ts
+++ src/client/config/assets.ts
@@ -66,7 +66,7 @@ export const ASSETS = {
       radius: 0.6,
       speed: {
         inAir: 30.0,
-        onGround: 70.0,
+        onGround: 60.0,
         boostMultiplier: 15.0
       },
       jumpHeight: 7.0,
