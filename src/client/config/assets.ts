fc981d1b6e99579643a4928e8ba6e6200ff84336
ali46000-source <ali46000@auburnschl.edu>
2026-06-05 09:54:54 -0400

    radius

--- src/client/config/assets.ts
+++ src/client/config/assets.ts
@@ -63,7 +63,7 @@ export const ASSETS = {
       scale: 1,
       mass: 1.0, // Standard weight
       height: 1.8,
-      radius: 1,
+      radius: 0.5,
       speed: {
         inAir: 90.0,
         onGround: 110.0,
