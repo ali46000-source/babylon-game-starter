89b41fb95ca799c4a8228c83e1b37c950b6b977f
ali46000-source <ali46000@auburnschl.edu>
2026-06-05 09:48:21 -0400

    fat red

--- src/client/config/assets.ts
+++ src/client/config/assets.ts
@@ -63,7 +63,7 @@ export const ASSETS = {
       scale: 1,
       mass: 1.0, // Standard weight
       height: 1.8,
-      radius: 0.6,
+      radius: 2.3,
       speed: {
         inAir: 90.0,
         onGround: 110.0,
