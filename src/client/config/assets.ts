269d44e0f9bda5fb1e12d72e3e9a270118458cc1
ali46000-source <ali46000@auburnschl.edu>
2026-06-05 09:35:54 -0400

    size

--- src/client/config/assets.ts
+++ src/client/config/assets.ts
@@ -863,7 +863,7 @@ export const ASSETS = {
       model:
         'https://raw.githubusercontent.com/EricEisaman/game-dev-1a/main/assets/models/environments/mansion/mansion.glb',
       lightmap: '',
-      scale: 10,
+      scale: 35,
       lightmappedMeshes: EMPTY_LIGHTMAPPED_MESHES,
       physicsObjects: EMPTY_PHYSICS_OBJECTS,
       sky: {
