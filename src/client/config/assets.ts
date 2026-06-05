62361185dde2ea20c32a9b76305b8eaeba03de97
ali46000-source <ali46000@auburnschl.edu>
2026-06-05 09:33:23 -0400

    spawn

--- src/client/config/assets.ts
+++ src/client/config/assets.ts
@@ -873,7 +873,7 @@ export const ASSETS = {
         BLUR: 0.2,
         TYPE: 'SPHERE' satisfies SkyType
       },
-      spawnPoint: new BABYLON.Vector3(0, 36, -20),
+      spawnPoint: new BABYLON.Vector3(0, 15, -20),
       spawnRotation: new BABYLON.Vector3(0, 0, 0)
     },
     {
