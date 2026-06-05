b4c34a7f09e51374acaf5c0ae97a17bf5e2caeb8
ali46000-source <ali46000@auburnschl.edu>
2026-06-05 10:15:30 -0400

    yhedyh

--- src/client/config/assets.ts
+++ src/client/config/assets.ts
@@ -168,7 +168,7 @@ export const ASSETS = {
         onGround: 120.0,
         boostMultiplier: 50.0
       },
-      jumpHeight: 50, // higher jumps
+      jumpHeight: 70, // higher jumps
       rotationSpeed: 0.04, // Slower rotation
       rotationSmoothing: 0.2,
       animationBlend: 200,
